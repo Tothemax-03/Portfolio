1bf9d7349b9e3c94bc87230e48d25cdbabd3a733
Paul Czar Cataylo <paulczarcatayloo03@gmail.com>
2026-03-13 21:40:02 +0800

    Update portfolio

--- Portfolio-main/src/constants/experience.ts
+++ Portfolio-main/src/constants/experience.ts
@@ -1,22 +1,28 @@
 import type { Experience } from "../types";
 
 export const experienceData: Experience[] = [
-    {
-    companyName: "Remotask",
-    jobTitle: "Freelance Data Annotator",
-    date: "2024",
+  {
+    companyName: "Academic Projects",
+    jobTitle: "BSIT Student Developer",
+    date: "2026",
     current: true,
   },
- {
-  companyName: "Academic Projects",
-  jobTitle: "BSIT Student Developer",
-  date: "2026",
-  current: false,
-},
   {
     companyName: "Negros Oriental State University",
     jobTitle: "BSIT Student | Web Development & UI/UX Projects",
     date: "2026",
+    current: false,
+  },
+  {
+    companyName: "Remotask",
+    jobTitle: "Freelance Data Annotator",
+    date: "2024",
+    current: false,
+  },
+  {
+    companyName: "ACSAT-BSCPE, Self Learning & Online Courses",
+    jobTitle: "Started Learning Programming | Computer Engineering & Programming Foundations",
+    date: "2023",
     current: false,
   },
 ];
